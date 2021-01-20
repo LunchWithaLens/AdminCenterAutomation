bb3fa00c5063b001995df401f8e78e17af5d9646
Brian Smith <Brian.Smith@microsoft.com>
2021-01-20 15:43:29 -0800

    Added trailing / after messages - reverted subject

--- OneHourSHD/run.ps1
+++ OneHourSHD/run.ps1
@@ -100,63 +100,7 @@ ForEach($channel in $channels){
             If($message.MessageType -eq 'Incident'){
                 If($message.WorkloadDisplayName -match $channel.product){
                     
-                
-                # Write-Host $request
-                # $teamId = $channel.teamId
-                # $teamChannelId = $channel.teamChannelId
-                $headers = @{}
-                $headers.Add('Authorization','Bearer ' + $graphToken.AccessToken)
-                # $headers.Add('If-Match', $freshEtagTaskContent.'@odata.etag')
-
-                ForEach($existingChannelMessages in $existingMessagesContentValues){
-                    # Write-Host $existingChannelMessages.id
-                    # Write-Host $existingChannelMessages.subject
-                    If($existingChannelMessages.subject){
-                        If($existingChannelMessages.subject.Contains($message.Id)){
-                        $reply = $true
-                        $fullMessage = '<at id=\"0\">' + $channel.contactName + '</at> - '
-                        ForEach($messagePart in $message.Messages){
-                            $fullMessage += $messagePart.MessageText
-                            }
-                        $setBody = @{}
-                        $setBody.Add("contentType", "html")
-                        $setBody.Add("content", $fullMessage)
-
-                        $userDetail = @{}
-                        $userDetail.Add("displayName", $channel.contactName)
-                        $userDetail.Add("id", $channel.contactAad)
-                        $userDetail.Add("userIdentityType", "aadUser")
-
-                        $user = @{}
-                        $user.Add("user", $userDetail)
-
-                        $mentions = @()
-                        $mentions += (@{
-                        id = 0;
-                        mentionText = $channel.contactName;
-                        mentioned = $user;
-                        })
-                
-                        $setPost = @{}
-                        # $setPost.Add("importance", "high")
-                        # $setPost.Add("subject", )    
-                        $SetPost.Add("body",$message.Id + " " + $message.Status + " " + $channel.product + " " + $message.Title + " " + $setBody)
-                        $setPost.Add("mentions",$mentions)
-                        $request = @"
-$($setPost | ConvertTo-Json -Depth 4)
-"@
-                $request = $request.Replace("\\\", "\")
-                        $uri = "https://graph.microsoft.com/beta/teams/" + $teamId + "/channels/" + $teamChannelId + "/messages" + $message.Id + "/replies"
-
-                        $result = Invoke-WebRequest -Uri $uri -Method Post `
-                            -Body $request -Headers $headers -UseBasicParsing `
-                            -ContentType "application/json"
-                        Write-Output "PowerShell script processed queue message REPLY " $message.id
-                        break parentloop
-                    } else {
-                    $reply = $false
-
-                    $fullMessage = '<at id=\"0\">' + $channel.contactName + '</at> - '
+                $fullMessage = '<at id=\"0\">' + $channel.contactName + '</at> - '
                 ForEach($messagePart in $message.Messages){
                     $fullMessage += $messagePart.MessageText
                     }
@@ -188,7 +132,28 @@ $($setPost | ConvertTo-Json -Depth 4)
 $($setPost | ConvertTo-Json -Depth 4)
 "@
                 $request = $request.Replace("\\\", "\")
+                # Write-Host $request
+                # $teamId = $channel.teamId
+                # $teamChannelId = $channel.teamChannelId
+                $headers = @{}
+                $headers.Add('Authorization','Bearer ' + $graphToken.AccessToken)
+                # $headers.Add('If-Match', $freshEtagTaskContent.'@odata.etag')
 
+                ForEach($existingChannelMessages in $existingMessagesContentValues){
+                    # Write-Host $existingChannelMessages.id
+                    # Write-Host $existingChannelMessages.subject
+                    If($existingChannelMessages.subject){
+                        If($existingChannelMessages.subject.Contains($message.Id)){
+                        $reply = $true
+                        $uri = "https://graph.microsoft.com/beta/teams/" + $teamId + "/channels/" + $teamChannelId + "/messages/" + $message.Id + "/replies"
+
+                        $result = Invoke-WebRequest -Uri $uri -Method Post `
+                            -Body $request -Headers $headers -UseBasicParsing `
+                            -ContentType "application/json"
+                        Write-Output "PowerShell script processed queue message REPLY " $message.id
+                        break parentloop
+                    } else {
+                    $reply = $false
                     $uri = "https://graph.microsoft.com/v1.0/teams/" + $teamId + "/channels/" + $teamChannelId + "/messages/"
 
                     $result = Invoke-WebRequest -Uri $uri -Method Post `
