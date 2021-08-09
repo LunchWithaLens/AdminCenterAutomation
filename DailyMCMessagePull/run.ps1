532be615accf587a2e5ed634d064ab0d51232739
Brian Smith <Brian.Smith@microsoft.com>
2021-08-09 15:05:17 -0700

    Typo

--- DailyMCMessagePull/run.ps1
+++ DailyMCMessagePull/run.ps1
@@ -78,7 +78,7 @@ $headers = @{}
 $headers.Add('Authorization','Bearer ' + $graphToken.AccessToken)
 $headers.Add('Content-Type', "application/json")
 
-$uri = "https://graph.microsoft.com/v1.0/admin/serviceAnnouncements/messages"
+$uri = "https://graph.microsoft.com/v1.0/admin/serviceAnnouncement/messages"
 
 $messages = Invoke-WebRequest -Uri $uri -Method Get -Headers $headers -UseBasicParsing
 
